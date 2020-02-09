e5d93cf8aa5421c453fd08221f82f211b3f2ef1f
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-02-09 22:26:12 +0900

    section-020の実装

---
+++ pages/authed-route.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>認証が必要なページ</h2>
+    <p>
+      Cookie の `credential` の値が Truthy の場合のみアクセス可能です<br>
+      <nuxt-link to="/">トップページへ戻る</nuxt-link>
+    </p>
+  </div>
+</template>
